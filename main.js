d32fb2cdc9b3b2cfb70241854f7b016670b34734
Koen Schmeets <k.schmeets@gmail.com>
2015-01-17 16:29:06 +0100

    Updating ENV checks

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 'use strict';
 
+var _ = require('underscore');
 var App = require('./lib/app');
 
-if ( ! process.env.POSTGRESQL_HOST)         throw new Error('The POSTGRESQL_HOST environment variable is not defined!');
-if ( ! process.env.POSTGRESQL_PORT)         throw new Error('The POSTGRESQL_PORT environment variable is not defined!');
-if ( ! process.env.POSTGRESQL_USER)         throw new Error('The POSTGRESQL_USER environment variable is not defined!');
-if ( ! process.env.POSTGRESQL_PASSWORD)     throw new Error('The POSTGRESQL_PASSWORD environment variable is not defined!');
-if ( ! process.env.ELASTICSEARCH_HOST)      throw new Error('The ELASTICSEARCH_HOST environment variable is not defined!');
-if ( ! process.env.QUEUE_CONNECTIONSTRING)  throw new Error('The QUEUE_CONNECTIONSTRING environment variable is not defined!');
-if ( ! process.env.LEVEL_PATH)              throw new Error('The LEVEL_PATH environment variable is not defined!');
-if ( ! process.env.SCHEMA_PATH)             throw new Error('The SCHEMA_PATH environment variable is not defined!');
+if ( ! _.isString(process.env.POSTGRESQL_HOST))         throw new Error('The POSTGRESQL_HOST environment variable is not defined!');
+if ( ! _.isString(process.env.POSTGRESQL_PORT))         throw new Error('The POSTGRESQL_PORT environment variable is not defined!');
+if ( ! _.isString(process.env.POSTGRESQL_USER))         throw new Error('The POSTGRESQL_USER environment variable is not defined!');
+if ( ! _.isString(process.env.POSTGRESQL_PASSWORD))     throw new Error('The POSTGRESQL_PASSWORD environment variable is not defined!');
+if ( ! _.isString(process.env.ELASTICSEARCH_HOST))      throw new Error('The ELASTICSEARCH_HOST environment variable is not defined!');
+if ( ! _.isString(process.env.QUEUE_CONNECTIONSTRING))  throw new Error('The QUEUE_CONNECTIONSTRING environment variable is not defined!');
+if ( ! _.isString(process.env.LEVEL_PATH))              throw new Error('The LEVEL_PATH environment variable is not defined!');
+if ( ! _.isString(process.env.SCHEMA_PATH))             throw new Error('The SCHEMA_PATH environment variable is not defined!');
 
 var config = require('./config');
 
