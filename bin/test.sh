a999b7ec10c659cd2e33ba95cc777393a8e4f093
Koen Schmeets <k.schmeets@gmail.com>
2015-01-19 05:41:54 +0100

    Simplifying test command

--- bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TAPE="$DIR/../node_modules/tape/bin/tape"
-TAPSPEC="$DIR/../node_modules/tap-spec/bin/cmd.js"
-
-node $TAPE tests/testSchemaClient.js tests/testSchemaService.js | node $TAPSPEC
