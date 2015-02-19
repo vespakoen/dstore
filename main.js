07c5621dcdfbc9b3ec4f7b29c616ae85d3e016f9
Koen Schmeets <k.schmeets@gmail.com>
2015-02-19 14:55:39 +0100

    Updating file paths

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ if ( ! _.isString(process.env.POSTGRESQL_PASSWORD))     throw new Error('The POS
 if ( ! _.isString(process.env.ELASTICSEARCH_HOST))      throw new Error('The ELASTICSEARCH_HOST environment variable is not defined!');
 if ( ! _.isString(process.env.QUEUE_CONNECTIONSTRING))  throw new Error('The QUEUE_CONNECTIONSTRING environment variable is not defined!');
 if ( ! _.isString(process.env.LEVEL_PATH))              throw new Error('The LEVEL_PATH environment variable is not defined!');
-if ( ! _.isString(process.env.BLUEPRINT_PATH))          throw new Error('The BLUEPRINT_PATH environment variable is not defined!');
+if ( ! _.isString(process.env.PROJECT_FILE_PATH))          throw new Error('The PROJECT_FILE_PATH environment variable is not defined!');
 if ( ! _.isString(process.env.PORT))                    throw new Error('The PORT environment variable is not defined!');
 
 var config = require('./config');
@@ -27,31 +27,36 @@ var app = new App({
   schemas: schemas
 });
 
-app.use(require('./lib/elasticsearch/elasticsearchClient'));
-app.use(require('./lib/elasticsearch/elasticsearchFacade'));
-app.use(require('./lib/elasticsearch/elasticsearchMigrator'));
-app.use(require('./lib/elasticsearch/elasticsearchRepository'));
-app.use(require('./lib/elasticsearch/elasticsearchSerializer'));
+app.use(require('./lib/storage/itemTransformer'));
 
-app.use(require('./lib/level/levelAdapter'));
-app.use(require('./lib/level/levelFacade'));
-app.use(require('./lib/level/levelRepository'));
-app.use(require('./lib/level/levelSerializer'));
+app.use(require('./lib/storage/elasticsearch/elasticsearchClient'));
+app.use(require('./lib/storage/elasticsearch/elasticsearchFacade'));
+app.use(require('./lib/storage/elasticsearch/elasticsearchMigrator'));
+app.use(require('./lib/storage/elasticsearch/elasticsearchRepository'));
+app.use(require('./lib/storage/elasticsearch/elasticsearchSerializer'));
 
-app.use(require('./lib/postgresql/postgresqlAdapter'));
-app.use(require('./lib/postgresql/postgresqlFacade'));
-app.use(require('./lib/postgresql/postgresqlMigrator'));
-app.use(require('./lib/postgresql/postgresqlRepository'));
-app.use(require('./lib/postgresql/postgresqlSerializer'));
+app.use(require('./lib/storage/level/levelAdapter'));
+app.use(require('./lib/storage/level/levelFacade'));
+app.use(require('./lib/storage/level/levelRepository'));
+app.use(require('./lib/storage/level/levelSerializer'));
 
-app.use(require('./lib/blueprint/client/fileBlueprintClient'));
-app.use(require('./lib/blueprint/blueprintAdapter'));
-app.use(require('./lib/blueprint/blueprintDiffer'));
-app.use(require('./lib/blueprint/blueprintFacade'));
-app.use(require('./lib/blueprint/blueprintService'));
-app.use(require('./lib/blueprint/blueprintSnapshotter'));
+app.use(require('./lib/storage/postgresql/postgresqlAdapter'));
+app.use(require('./lib/storage/postgresql/postgresqlFacade'));
+app.use(require('./lib/storage/postgresql/postgresqlMigrator'));
+app.use(require('./lib/storage/postgresql/postgresqlRepository'));
+app.use(require('./lib/storage/postgresql/postgresqlSerializer'));
 
-app.use(require('./lib/item/itemTransformer'));
+app.use(require('./lib/project/blueprint/blueprintFacade'));
+app.use(require('./lib/project/blueprint/blueprintService'));
+app.use(require('./lib/project/blueprint/blueprintDiffer'));
+app.use(require('./lib/project/client/projectClient'));
+app.use(require('./lib/project/client/fileProjectClient'));
+app.use(require('./lib/project/client/levelProjectClient'));
+app.use(require('./lib/project/client/postgresqlProjectClient'));
+app.use(require('./lib/project/projectFacade'));
+app.use(require('./lib/project/projectService'));
+app.use(require('./lib/project/projectTagger'));
+
 
 app.use(require('./lib/queue'));
 app.use(require('./lib/validator'));
