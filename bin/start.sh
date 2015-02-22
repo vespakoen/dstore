3f87dd27258ee040ccba671eb3f9e2c6e9540678
Koen Schmeets <k.schmeets@gmail.com>
2015-02-22 01:56:16 +0100

    projector -> dstore

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pm2 start $PROJECTOR_PATH/bin/projector.js
+pm2 start $PROJECTOR_PATH/bin/dstore.js
 pm2 start $PROJECTOR_PATH/bin/store.js -f -- --store elasticsearch
 pm2 start $PROJECTOR_PATH/bin/store.js -f -- --store level
 pm2 start $PROJECTOR_PATH/bin/store.js -f -- --store postgresql
