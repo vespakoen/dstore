82d34cfe12196376bb9e3c488a5c3eab9a8e9607
Koen Schmeets <k.schmeets@gmail.com>
2015-03-03 00:33:02 +0100

    Updating config

--- config.js
+++ config.js
@@ -1,15 +1,5 @@
 var _ = require('underscore');
 
-if ( ! _.isString(process.env.POSTGRESQL_HOST))         throw new Error('The POSTGRESQL_HOST environment variable is not defined!');
-if ( ! _.isString(process.env.POSTGRESQL_PORT))         throw new Error('The POSTGRESQL_PORT environment variable is not defined!');
-if ( ! _.isString(process.env.POSTGRESQL_USER))         throw new Error('The POSTGRESQL_USER environment variable is not defined!');
-if ( ! _.isString(process.env.POSTGRESQL_PASSWORD))     throw new Error('The POSTGRESQL_PASSWORD environment variable is not defined!');
-if ( ! _.isString(process.env.ELASTICSEARCH_HOST))      throw new Error('The ELASTICSEARCH_HOST environment variable is not defined!');
-if ( ! _.isString(process.env.QUEUE_CONNECTIONSTRING))  throw new Error('The QUEUE_CONNECTIONSTRING environment variable is not defined!');
-if ( ! _.isString(process.env.LEVEL_PATH))              throw new Error('The LEVEL_PATH environment variable is not defined!');
-if ( ! _.isString(process.env.PROJECT_FILE_PATH))       throw new Error('The PROJECT_FILE_PATH environment variable is not defined!');
-if ( ! _.isString(process.env.PORT))                    throw new Error('The PORT environment variable is not defined!');
-
 module.exports = {
   stores: process.env.STORES ? process.env.STORES.split(',') : [
     'elasticsearch',
@@ -31,7 +21,7 @@ module.exports = {
   },
 
   level: {
-    path: process.env.LEVEL_PATH || '/tmp/'
+    path: process.env.LEVEL_PATH || '/tmp'
   },
 
   queue: {
@@ -44,7 +34,7 @@ module.exports = {
   project: {
     client: process.env.PROJECT_CLIENT || 'file',
     file: {
-      path: process.env.PROJECT_FILE_PATH
+      path: process.env.PROJECT_FILE_PATH || '/tmp/project'
     }
   }
 };
